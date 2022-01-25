71acf06d755e425dcbbcc024b8a486c853a5ea99
Hans Tool <hans@defect.org>
2022-01-25 11:48:46 +0100

    Some changes to ensure css reloads and has 'day' fallback.  Please enter the commit message for your changes. Lines starting

--- Content/cssmode.js
+++ Content/cssmode.js
@@ -1,7 +1,14 @@
 async function asyncData() {
-  const response = await fetch('http://mfdstore.navico.com:3000/signalk/v1/api/vessels/self/environment/mode/value/');
-  const dayOrNight = await response.json(); //extract JSON from the http response
-  console.log("dayOrNight: " + dayOrNight);
+  var dayOrNight;
+  try {
+    const response = await fetch('http://mfdstore.navico.com:3000/signalk/v1/api/vessels/self/environment/mode/value/');
+    dayOrNight = await response.json(); //extract JSON from the http response
+    console.log("dayOrNight: " + dayOrNight);
+  } catch (err) {
+    console.log("Failed to retrieve from SignalK. Setting to day");
+    dayOrNight = 'day';
+  }
+
   var cssmode = document.createElement('link');
   cssmode.type = 'text/css';
   cssmode.rel = 'stylesheet';
